a432db91d5a58e66bc99cc72f6c7f80552703ec4
JStrukt <jochen@struktu.red>
2023-02-09 11:02:47 +0100

    Removed everything related to non-python support

--- pydantic_flatbuffers/bin/flatc.py
+++ pydantic_flatbuffers/bin/flatc.py
@@ -5,13 +5,7 @@
 import argparse
 
 from pydantic_flatbuffers.fbs.parser import load
-from pydantic_flatbuffers.lang.cpp.generate import generate_cpp
-from pydantic_flatbuffers.lang.java.generate import generate_ijava
-from pydantic_flatbuffers.lang.kt.generate import generate_kt
 from pydantic_flatbuffers.lang.py.generate import generate_py
-from pydantic_flatbuffers.lang.rust.generate import generate_rust
-from pydantic_flatbuffers.lang.swift.generate import generate_swift
-from pydantic_flatbuffers.lang.yaml.generate import generate_yaml
 
 
 def main():
@@ -22,22 +16,8 @@ def main():
     parser.add_argument(
         "--templates", action="store", nargs="+", help="Filename to template"
     )
-    parser.add_argument("--cpp", type=bool, default=False, help="Generate C++ code")
-    parser.add_argument(
-        "--ijava", type=bool, default=False, help="Generate Java interface code"
-    )
-    parser.add_argument("--yaml", type=bool, default=False, help="Generate Yaml code")
     parser.add_argument(
         "--python", type=bool, default=False, help="Generate Python code"
-    )
-    parser.add_argument(
-        "--kotlin", type=bool, default=False, help="Generate Kotlin code"
-    )
-    parser.add_argument(
-        "--swift", type=bool, default=False, help="Generate Swift code"
-    )
-    parser.add_argument(
-        "--rust", type=bool, default=False, help="Generate Rust code"
     )
     # TODO: pass args.sort to parser
     parser.add_argument(
@@ -45,32 +25,11 @@ def main():
     )
     args, rest = parser.parse_known_args()
     for filename in rest:
-        if args.cpp:
-            generate_cpp(filename, load(filename))
-        if args.ijava:
-            generate_ijava(filename, load(filename))
-        if args.yaml:
-            generate_yaml(filename, load(filename))
         if args.python:
             if args.templates:
                 generate_py(filename, load(filename), args.templates)
             else:
                 generate_py(filename, load(filename))
-        if args.kotlin:
-            if args.templates:
-                generate_kt(filename, load(filename), args.templates)
-            else:
-                generate_kt(filename, load(filename))
-        if args.rust:
-            if args.templates:
-                generate_rust(filename, load(filename), args.templates)
-            else:
-                generate_rust(filename, load(filename))
-        if args.swift:
-            if args.templates:
-                generate_swift(filename, load(filename), args.templates)
-            else:
-                generate_swift(filename, load(filename))
 
 
 if __name__ == "__main__":
--- tests/test_flatc.py
+++ tests/test_flatc.py
@@ -3,10 +3,7 @@ from shutil import rmtree
 import unittest
 
 from pydantic_flatbuffers.fbs.parser import load
-from pydantic_flatbuffers.lang.kt.generate import generate as generate_kt
 from pydantic_flatbuffers.lang.py.generate import generate as generate_py
-from pydantic_flatbuffers.lang.rust.generate import generate as generate_rust
-from pydantic_flatbuffers.lang.swift.generate import generate as generate_swift
 from pydantic_flatbuffers.lang import generate_files
 from pathlib import Path
 from .parser_cases import COLOR
@@ -23,24 +20,6 @@ class CodeGeneratorTests(unittest.TestCase):
 
     def tearDown(self):
         rmtree(self.COLOR_DIR)
-
-    def test_rust(self):
-        generate_files(*generate_rust(self.TEST_CASE, load(self.TEST_CASE)))
-        rust_if: Path = self.COLOR_DIR / "color.rs"
-        rust_ref: Path = self.TESTS_DIR / "expected" / "golden-color.rs"
-        self.assertEqual(rust_ref.read_text(), rust_if.read_text())
-
-    def test_kotlin(self):
-        generate_files(*generate_kt(self.TEST_CASE, load(self.TEST_CASE)))
-        kt_if: Path = self.COLOR_DIR / "color.kt"
-        kt_ref: Path = self.TESTS_DIR / "expected" / "golden-color.kt"
-        self.assertEqual(kt_if.read_text(), kt_ref.read_text())
-
-    def test_swift(self):
-        generate_files(*generate_swift(self.TEST_CASE, load(self.TEST_CASE)))
-        swift_if: Path = self.COLOR_DIR / "color.swift"
-        swift_ref: Path = self.TESTS_DIR / "expected" / "golden-color.swift"
-        self.assertEqual(swift_if.read_text(), swift_ref.read_text())
 
     def test_py(self):
         generate_files(*generate_py(self.TEST_CASE, load(self.TEST_CASE)))
